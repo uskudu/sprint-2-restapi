ef44690d8057c0578764ae7835e71444f3649a6f
uskudu <usskudu@gmail.com>
2026-02-15 02:07:21 +0300

    get task by id

--- internal/database/tasks.go
+++ internal/database/tasks.go
@@ -1,6 +1,8 @@
 package database
 
 import (
+	"database/sql"
+	"errors"
 	"fmt"
 	"sptringTwoRestAPI/internal/models"
 
@@ -28,4 +30,23 @@ order by created_at desc;`
 		return nil, fmt.Errorf("error getting all tasks: %w", err)
 	}
 	return tasks, nil
+}
+
+func (s *TaskStore) GetByID(id int) (*models.Task, error) {
+	var task models.Task
+
+	query := `
+select id, title, description, completed, created_at, updated_at 
+from tasks 
+where id = $1;`
+
+	err := s.db.Get(&task, query, id)
+
+	if errors.Is(err, sql.ErrNoRows) {
+		return nil, fmt.Errorf("task with id %d not found: %w", id, err)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("error getting task by id: %w", err)
+	}
+	return &task, nil
 }
