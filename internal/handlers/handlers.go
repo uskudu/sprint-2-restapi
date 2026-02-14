ed5f9a2d536f471fc67af59196177ff7c379761a
uskudu <usskudu@gmail.com>
2026-02-15 02:34:08 +0300

    done handler get all tasks

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -2,6 +2,7 @@ package handlers
 
 import (
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"sptringTwoRestAPI/internal/database"
 )
@@ -20,6 +21,15 @@ func respondWithJSON(w http.ResponseWriter, statusCode int, payload any) {
 	json.NewEncoder(w).Encode(payload)
 }
 
-func respondWithError(w http.ResponseWriter, statusCode int, message string) {
-	respondWithJSON(w, statusCode, map[string]string{"error": message})
+func respondWithError(w http.ResponseWriter, statusCode int, err error) {
+	respondWithJSON(w, statusCode, map[string]error{"error": err})
+}
+
+func (h *Handlers) GetAllTasks(w http.ResponseWriter, r *http.Request) {
+	tasks, err := h.store.GetAll()
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, fmt.Errorf("error getting all tasks: %w", err))
+		return
+	}
+	respondWithJSON(w, http.StatusOK, tasks)
 }
