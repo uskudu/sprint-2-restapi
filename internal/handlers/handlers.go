f8a2169c4978c46cf967f3b4c5d62ef6c17c9a96
uskudu <usskudu@gmail.com>
2026-02-15 18:23:56 +0300

    done get task handler

--- internal/handlers/handlers.go
+++ internal/handlers/handlers.go
@@ -5,6 +5,8 @@ import (
 	"fmt"
 	"net/http"
 	"sptringTwoRestAPI/internal/database"
+	"strconv"
+	"strings"
 )
 
 type Handlers struct {
@@ -21,6 +23,7 @@ func respondWithJSON(w http.ResponseWriter, statusCode int, payload any) {
 	json.NewEncoder(w).Encode(payload)
 }
 
+// todo err error is message string at 1:14:14
 func respondWithError(w http.ResponseWriter, statusCode int, err error) {
 	respondWithJSON(w, statusCode, map[string]error{"error": err})
 }
@@ -28,7 +31,33 @@ func respondWithError(w http.ResponseWriter, statusCode int, err error) {
 func (h *Handlers) GetAllTasks(w http.ResponseWriter, r *http.Request) {
 	tasks, err := h.store.GetAll()
 	if err != nil {
-		respondWithError(w, http.StatusInternalServerError, fmt.Errorf("error getting all tasks: %w", err))
+		respondWithError(
+			w, http.StatusInternalServerError,
+			fmt.Errorf("error getting all tasks: %w", err),
+		)
+		return
+	}
+	respondWithJSON(w, http.StatusOK, tasks)
+}
+
+func (h *Handlers) GetTask(w http.ResponseWriter, r *http.Request) {
+	pathParts := strings.Split(strings.TrimPrefix(r.URL.Path, "/tasks/"), "/")
+
+	id, err := strconv.Atoi(pathParts[0])
+	if err != nil {
+		respondWithError(
+			w, http.StatusInternalServerError,
+			fmt.Errorf("error converting path string to task id: %w", err),
+		)
+		return
+	}
+
+	tasks, err := h.store.GetByID(id)
+	if err != nil {
+		respondWithError(
+			w, http.StatusInternalServerError,
+			fmt.Errorf("error getting a task: %w", err),
+		)
 		return
 	}
 	respondWithJSON(w, http.StatusOK, tasks)
