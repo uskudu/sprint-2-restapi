50884a3b58ec6334ce7ab87852d8023579328cd2
uskudu <usskudu@gmail.com>
2026-02-15 19:13:00 +0300

    finis

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,5 +1,79 @@
 package main
 
-func main() {
+import (
+	"log"
+	"net/http"
+	"os"
+	"sptringTwoRestAPI/internal/database"
+	"sptringTwoRestAPI/internal/handlers"
+)
 
+func main() {
+	databaseURL := os.Getenv("DATABASE_URL")
+	if databaseURL == "" {
+		databaseURL = "postgres://tu:tp@localhost:5433/tdb?sslmode=disable"
+	}
+	serverPort := os.Getenv("SERVER_PORT")
+	if serverPort == "" {
+		serverPort = "8081"
+	}
+	log.Printf("starting server on port: %s", serverPort)
+
+	db, err := database.Connect(databaseURL)
+	if err != nil {
+		log.Fatalf("database didnt connect: %s", err)
+	}
+	log.Printf("database successfully connected: %s", databaseURL)
+	defer db.Close()
+
+	taskStore := database.NewTaskStore(db)
+	handler := handlers.NewHandlers(taskStore)
+
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("/tasks", methodHandler(handler.GetAllTasks, http.MethodGet))
+	mux.HandleFunc("/tasks/create", methodHandler(handler.GetAllTasks, http.MethodPost))
+	mux.HandleFunc("/tasks/", taskIDHandler(handler))
+
+	loggedMux := loggingMiddleware(mux)
+
+	serverAddr := ":" + serverPort
+
+	err = http.ListenAndServe(serverAddr, loggedMux)
+	if err != nil {
+		log.Fatalf("server couldnt start: %s", err)
+	} else {
+		log.Printf("server started at %s", serverAddr)
+	}
+}
+
+func methodHandler(handlerFunc http.HandlerFunc, allowedMethod string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != allowedMethod {
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		}
+		handlerFunc(w, r)
+	}
+}
+
+func taskIDHandler(handler *handlers.Handlers) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		switch r.Method {
+		case http.MethodGet:
+			handler.GetTask(w, r)
+		case http.MethodPut:
+			handler.UpdateTask(w, r)
+		case http.MethodDelete:
+			handler.DeleteTask(w, r)
+		default:
+			http.Error(w, http.StatusText(http.StatusMethodNotAllowed), http.StatusMethodNotAllowed)
+		}
+	}
+}
+
+func loggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("%s %s %s", r.Method, r.URL.Path, r.RemoteAddr)
+		next.ServeHTTP(w, r)
+	})
 }
